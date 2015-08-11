32a80eda50ff9c20c95e101759d29e676b81d94e
David Kelley <dkelley@frbchi.org>
2015-08-11 11:23:03 -0500

    Added descriptions when listing CHIDATA series

--- +cbd/+private/list.m
+++ +cbd/+private/list.m
@@ -1,12 +1,19 @@
-function list(getDocs)
+function series = list
 % List CHIDATA series
 
-if nargin == 0 
-    getDocs = false;
-end
+% David Kelley, 2015
 
 chidataDir = 'O:\PROJ_LIB\Presentations\Chartbook\Data\CHIDATA\';
 
 seriesList = readtable([chidataDir 'index.csv']);
 
-series = seriesList{:,1};
+% Opens each file and reads the descriptions
+series = {};
+files = unique(seriesList{:,2});
+for iF = 1:length(files)
+    readData = readtable([chidataDir files{iF} '_prop.csv'], 'ReadRowNames', true);
+    seriesNames = readData.Properties.VariableNames;
+    descripInd = find(strcmpi(readData.Properties.RowNames, 'Description'), 1);
+    descrips = readData{descripInd,:};
+    series = [series; [seriesNames' descrips'] ]; %#ok<AGROW>
+end
