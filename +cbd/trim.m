2a01232d6abc4902d2becf0a441ce5c45e4f9b4d
David Kelley <dkelley@frbchi.org>
2016-03-21 09:44:24 -0500

    Add 'Inclusive' option to trim.

--- +cbd/trim.m
+++ +cbd/trim.m
@@ -1,35 +1,57 @@
 function inTable = trim(inTable, varargin)
 % TRIM Returns data between given a startDate or endDate
 %
-% trimmedData = trim(inTable, 'startDate', sDate) returns the data 
+% trimmedData = trim(inTable, 'startDate', sDate) returns the data
 % following the start date
 %
-% trimmedData = trim(inTable, 'endDate', eDate) returns the data 
+% trimmedData = trim(inTable, 'endDate', eDate) returns the data
 % preceeding the end date
+%
+% trimmedData = trim(inTable, 'Inclusive', true, ...) returns the smallest
+% data range which includes the start and end dates passed.
 
 % David Kelley, 2015
 
 %% Handle inputs
+firstDate = datenum(inTable.Properties.RowNames{1});
+lastDate = datenum(inTable.Properties.RowNames{end});
+
 inP = inputParser;
 dateValid = @(x) validateattributes(x, {'numeric', 'char'}, {'vector'});
-inP.addParameter('startDate', [], dateValid);
-inP.addParameter('endDate', [], dateValid);
+inP.addParameter('startDate', firstDate, dateValid);
+inP.addParameter('endDate', lastDate, dateValid);
+inP.addParameter('Inclusive', false, @islogical);
 
 inP.parse(varargin{:});
 opts = inP.Results;
 
-if ~isempty(opts.startDate)
-    if ~isnumeric(opts.startDate)
-        opts.startDate = datenum(opts.startDate);
-    end
-    dates = datenum(inTable.Properties.RowNames);
-    inTable = inTable(dates >= opts.startDate, :);
+if ~isnumeric(opts.startDate)
+  opts.startDate = datenum(opts.startDate);
 end
 
-if ~isempty(opts.endDate)
-    if ~isnumeric(opts.endDate)
-        opts.endDate = datenum(opts.endDate);
-    end
-    dates = datenum(inTable.Properties.RowNames);
-    inTable = inTable(dates <= opts.endDate, :);
+if ~isnumeric(opts.endDate)
+  opts.endDate = datenum(opts.endDate);
 end
+
+dates = datenum(inTable.Properties.RowNames);
+if ~opts.Inclusive
+  % Get dates starting at or after startDate and before or on endDate
+  sInd = find(dates < opts.startDate, 1, 'last') + 1;
+  eInd = find(dates > opts.endDate, 1, 'first')  - 1;
+else
+  % Get range that includes startDate and endDate
+  sInd = find(dates <= opts.startDate, 1, 'last');
+  eInd = find(dates >= opts.endDate, 1, 'first');
+end
+
+sInd(sInd > size(dates,1)) = size(dates,1);
+eInd(eInd < 1) = 1;
+eInd(isempty(eInd)) = size(dates,1);
+sInd(isempty(sInd)) = 1;
+
+dates = cbd.private.tableDates(inTable);
+inTable = inTable(sInd:eInd,:);
+inTable.Properties.UserData = dates(sInd:eInd,:);
+
+
+
