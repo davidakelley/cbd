4342ff4e68140bc56aeaac9583b02f0ce9c3e8e5
David A. Kelley <dkelley@frbchi.org>
2018-02-06 09:03:22 -0600

    Add functions for filling backwards and forwards in simple differences.

---
+++ +cbd/bfdiff.m
@@ -0,0 +1,25 @@
+function outData = bfdiff(series, backfillLevels)
+%BFGR Extends a series backward by using the differences in another series
+%
+% outData = bfdiff(series, backfillLevels) extends SERIES as far back as
+% backfillLevels goes by usng the differences of that series.
+
+% David Kelley, 2018
+
+%% Add back levels off of growth rates
+mergeData = cbd.merge(series, backfillLevels);
+back_data = mergeData{:,end};
+back_diff = [nan; back_data(2:end) - back_data(1:end-1)];
+
+outData = mergeData(:,1:end-1);
+
+for iSer = 1:size(outData, 2)
+    iData = outData{:,iSer};
+    firstLev = find(~isnan(iData), 1, 'first');
+    
+    for iLvl = firstLev-1:-1:1
+        iData(iLvl) = iData(iLvl+1) - back_diff(iLvl+1);
+    end
+    
+    outData{:,iSer} = iData;
+end
---
+++ +cbd/ffdiff.m
@@ -0,0 +1,25 @@
+function outData = ffdiff(series, backfillLevels)
+%FFGR Extends a series forward by using the differences in another series
+%
+% outData = ffdiff(series, fillLevels) extends SERIES as far forward as
+% fillLevels goes by usng the differences of that series.
+
+% David Kelley, 2015
+
+%% Add back levels off of growth rates
+mergeData = cbd.merge(series, backfillLevels);
+back_data = mergeData{:,end};
+fwd_diff = [nan; back_data(2:end) - back_data(1:end-1)];
+
+outData = mergeData(:,1:end-1);
+
+for iSer = 1:size(outData, 2)
+    iData = outData{:,iSer};
+    lastLev = find(~isnan(iData), 1, 'last');
+    
+    for iLvl = lastLev+1:size(iData, 1)
+        iData(iLvl) = iData(iLvl-1) + fwd_diff(iLvl);
+    end
+    
+    outData{:,iSer} = iData;
+end
