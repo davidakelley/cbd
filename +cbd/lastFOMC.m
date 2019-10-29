cb47e074d92afeed27993a518cd06cc40fed348c
Vamsi Kurakula <g1vxk03@economic.research>
2019-10-29 14:29:55 -0500

    Fix lastFOMC to include only the first day of two-day meetings

--- +cbd/lastFOMC.m
+++ +cbd/lastFOMC.m
@@ -26,13 +26,11 @@ function LastFOMCDate = lastFOMC(varargin)
 % Written by Vamsi Kurakula
 % Last Updated 2/5/2019
 
-
-
 % Making sure that if there are input arguments that they are the correct
 % length and type
 
 if isempty(varargin)
-    DateOfInterest = datetime('today');
+    DateOfInterest = datetime('today') - calyears(1);
 elseif length(varargin) == 1         
     if isdatetime(varargin{1})
         DateOfInterest = varargin{1}; 
@@ -40,46 +38,61 @@ elseif length(varargin) == 1
         try 
             DateOfInterest = datetime(varargin,'InputFormat','dd-MMM-yyyy');    
         catch
-            error('LastFOMC:NotADate', 'Function only accepts a single date input');
+            error('LastFOMC:NotADate', 'Function only accepts a single date input in format dd-MMM-yyyy.');
         end % End Try 
-    end  % End if for making sure varargin is a datetime 
-else    
-    error('LastFOMC:MultipleInputs', 'Function only accepts a single date input');   
+    end  % End if for making sure varargin is a datetime   
+    
 end % End if for length of inputs of function  
-    
-    
-% Setting StartDate
-StartDate = datestr(DateOfInterest - calyears(1));
+
+startDate = datestr(DateOfInterest);
+endDate = datenum(datetime('today'));
 
 % Collecting the data
-fomcdata = cbd.data('FOMC@DAILY','startDate',StartDate,'endDate',datenum(datetime('today')));
+fomcdata = cbd.data('FOMC@DAILY','startDate', startDate,'endDate', endDate);
+fomcdata.Date = fomcdata.Properties.RowNames;
 
-% Cutting out Things that are not FOMC Dates
-fomcdata(fomcdata.FOMC == -1,:) = [];
-fomcdata.Date = datetime(fomcdata.Row);
+LastFOMCDate = {};
+fomcIdxCounter = 1;
 
-% We only care abput collecting the first of the two consecutive dates 
-IsFirst = (fomcdata.Date(2:end) - fomcdata.Date(1:end-1)) < days(10);
+for meetingIdx = 2:height(fomcdata)
+    previous = fomcdata(meetingIdx - 1,:);
+    current  = fomcdata(meetingIdx, :);
 
-% dealing with the last date in the list separately 
-if (fomcdata.Date(end)  - fomcdata.Date(end-1)) < days(10)
-   IsFirst = [IsFirst; false];
-else
-   IsFirst = [IsFirst; true];
+    previousIsFOMC = previous.FOMC;
+    currentIsFOMC = current.FOMC; 
+    if currentIsFOMC == 1 && previousIsFOMC == 1
+         LastFOMCDate{fomcIdxCounter, 1} = previous.Date{:};
+         fomcIdxCounter = fomcIdxCounter + 1;
+    end
 end
 
-% Cutting out the extra FOMC dates
-fomcdata(~IsFirst,:) = [];
+% TODO: add a switch to add this portion 
+%       this counts ANY meeting, but if there are an official 
+%       meeting with back to back days, only returns the first one. 
+
+% % Cutting out Things that are not FOMC Dates
+% fomcdata(fomcdata.FOMC == -1,:) = [];
+% fomcdata.Date = datetime(fomcdata.Row);
+% 
+% % We only care abput collecting the first of the two consecutive dates 
+% IsFirst = (fomcdata.Date(2:end) - fomcdata.Date(1:end-1)) < days(10);
+% 
+% % dealing with the last date in the list separately 
+% if (fomcdata.Date(end)  - fomcdata.Date(end-1)) < days(10)
+%    IsFirst = [IsFirst; false];
+% else
+%    IsFirst = [IsFirst; true];
+% end
+% 
+% % Cutting out the extra FOMC dates
+% fomcdata(~IsFirst,:) = [];
 
 
 % if there is no inputs we want to return only the last FOMC Date
 % Otherwise we want to collect dates after the date of interest (input)
 
+
 if isempty(varargin)    
-     LastFOMCDate = fomcdata.Row(end);
-else    
-     LastFOMCDate = fomcdata.Row(datetime(fomcdata.Row) >= DateOfInterest);
+     LastFOMCDate = LastFOMCDate(end);
 end %End if
-
-
 end % End Function 
