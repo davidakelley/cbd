8aec06d9c9c33249c84af92fe48a20d26c116874
Santiago Sordo-Palacios <ssordopalacios@frbchi.org>
2019-12-12 15:48:18 -0600

    Fix ignoreNan bug in subtraction, multiplication, division

--- +cbd/division.m
+++ +cbd/division.m
@@ -1,17 +1,15 @@
 function divided = division(dataA, dataB, varargin)
-% Find the quotient of two 
-% DIVISION Finds the quotient of two series. 
+%DIVISION Finds the quotient of two series. 
 % 
-% divided = DIVISION(seriesA, seriesB) divides seriesA by seriesB
-% 
+% USAGE:
+% divided = DIVISION(seriesA, seriesB) divides seriesA by seriesB 
 % divided = DIVISION(series, constant) divides a series by a constant
-%
 % divided = DIVISION(constant, series) divides a constant by a series
-%
 % divided = DIVISION(constantA, constantB) divides constantA by constantB
-
+%
 % David Kelley, 2014
 
-divided = cbd.private.multiseriesFunction(dataA, dataB, @(x,y) rdivide(x,y));
+divided = cbd.private.multiseriesFunction( ...
+    dataA, dataB, @(x,y) rdivide(x,y), varargin{:});
 
-end
+end % function
